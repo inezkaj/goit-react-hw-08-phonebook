3b44d4bba413c9b467e1bd17e9590ee0ebe8c024
inezkaj <ienzkajed@icloud.com>
2024-03-07 17:19:31 +0100

    feat: homework08

--- src/components/UserMenu/LogoutButton.jsx
+++ src/components/UserMenu/LogoutButton.jsx
@@ -3,7 +3,6 @@ import { logout } from '../../redux/auth/operations';
 import useAuth from '../../hooks/useAuth';
 
 const LogoutButton = () => {
-  const { isLoggedIn } = useAuth();
   const dispatch = useDispatch();
   const handleLogout = () => {
     dispatch(logout());
